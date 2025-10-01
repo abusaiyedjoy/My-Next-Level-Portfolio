fea515c1ddb0322b4558057c160b995e99102863
abusaiyedjoy <abusaiyedjoy1@gmail.com>
2025-10-01 10:58:29 +0600

    Initial backend project setup with prisma

---
+++ Server/prisma/migrations/20250930195644_init/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Projects" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "Projects_pkey" PRIMARY KEY ("id")
+);
---
+++ Server/src/app.ts
@@ -0,0 +1,37 @@
+// import compression from "compression";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+// Middleware
+app.use(cors()); 
+// app.use(compression()); 
+app.use(express.json()); 
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+// app.use("/api/v1/user", userRouter);
+// app.use("/api/v1/post", postRouter);
+// app.use("/api/v1/auth", authRouter);
+
+
+app.get("/", (_req, res) => {
+  res.send("API is running");
+});
+
+
+// 404 Handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+export default app;
